48d0cc88318210e5e6c7b78eee0e026f5d214bae
Skander Ben Abdelmalak <skander@iubenda.com>
2023-12-21 14:09:45 +0100

    refactor: example project

---
+++ example/src/BaseLayout.tsx
@@ -0,0 +1,28 @@
+// BaseLayout.tsx
+import React from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+
+const BaseLayout = ({ children }) => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#f0f0f0', // You can choose a color that fits your app theme
+  },
+  container: {
+    flex: 1,
+    paddingHorizontal: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffff', // Background color for the inner container
+    // You can add more styling as needed
+  },
+});
+
+export default BaseLayout;
